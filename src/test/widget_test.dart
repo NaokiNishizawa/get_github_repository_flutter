4836fa267ed27d8338c030ed06373e1e3c958fdf
Naoki Nishizawa <mino24zawawork@gmail.com>
2023-06-03 03:24:21 +0900

    create widget test

--- src/test/widget_test.dart
+++ src/test/widget_test.dart
@@ -5,26 +5,50 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'dart:ui';
 
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:src/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  setUp(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    DartPluginRegistrant.ensureInitialized();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    const TEST_MOCK_STORAGE = './test/fixtures/core';
+    const channel = MethodChannel(
+      'plugins.flutter.io/path_provider',
+    );
+    channel.setMockMethodCallHandler((MethodCall MethodCall) async {
+      return TEST_MOCK_STORAGE;
+    });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await initHiveForFlutter();
+    await dotenv.load(fileName: 'assets/.env');
+  });
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('get github repository test', (WidgetTester tester) async {
+    const widget = ProviderScope(child: MyApp());
+
+    await tester.runAsync(() async {
+      await tester.pumpWidget(widget);
+
+      // 検索窓に"aaa"という文字列を入力する
+      await tester.enterText(find.byType(TextField), 'aaa');
+      expect(find.text('aaa'), findsOneWidget);
+
+      // キーボードの完了を押下し検索を開始する
+      await tester.testTextInput.receiveAction(TextInputAction.done);
+
+      // 結果を確認する
+      await tester.pumpAndSettle();
+      expect(find.byType(CircularProgressIndicator), findsNothing);
+      expect(find.byType(Scrollable), findsOneWidget);
+    });
   });
 }
